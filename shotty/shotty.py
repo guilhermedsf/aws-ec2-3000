febeaf235fe80034aa853c5cd132fcd644e1a0fc
guilhermedsf <41974221+guilhermedsf@users.noreply.github.com>
2020-06-09 22:05:16 -0700

    Open and running, list instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,10 +1,26 @@
 import boto3
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+#abrindo uma ssesao no aws
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+def list_instances():
     for i in ec2.instances.all():
-        print(i) 
+        tags = i.tags
+        print(tags,', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+        ))) 
+    return
+
+if __name__ == '__main__':
+    list_instances()
+    
+    
 
 
 
